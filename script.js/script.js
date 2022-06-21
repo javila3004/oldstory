25dc1d1bf0c3f47ca1343f97d74b83ae8a671b17
Juan Esteban Avila <avilareinajuanestaban@gmail.com>
2022-06-21 11:30:06 -0500

    pagina contacto terminada

--- script.js/script.js
+++ script.js/script.js
@@ -1,18 +1,4 @@
-document.querySelector(".bars__menu").addEventListener("clik",animateBars);
-
-var line1__bars=document.querySelector("line1__bars-menu");
-var line2__bars=document.querySelector("line2__bars-menu");
-var line3__bars=document.querySelector("line3__bars-menu");
-
-
-function animateBars(){
-    line1__bars.classList.toggle(activeline1__bars-menu);
-    line2__bars.classList.toggle(activeline2__bars-menu);
-    line3__bars.classList.toggle(activeline3__bars-menu);
-
-    container__menu.classList.toggle("menu__active");
-}
-var inputs =document.getElementsByClassName('formulario__input');
+let  inputs =document.getElementsByClassName('formulario__input');
 for (var i =0; i < inputs.length; i++){
     inputs[i].addEventListener('keyup', function(){
         if(this.value.length>=0){
@@ -22,3 +8,7 @@ for (var i =0; i < inputs.length; i++){
         }
     });
 }
+let  enviar = document.querySelector(".formulario__submit");
+enviar.addEventListener("click",() =>{
+    alert("Recibimos la Informacion")
+})
